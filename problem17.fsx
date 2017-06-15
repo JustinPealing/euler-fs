a56389a2119f89f3108fa2d74a254f4c373e533d
Justin Pealing <justin@pealing.me.uk>
2017-06-15 22:06:02 +0100

    Slightly tidyer  handling of "and"

--- problem17.fsx
+++ problem17.fsx
@@ -28,9 +28,11 @@ let rec toWords n =
     | x when x / 10 = 7 -> "seventy"::toWords (x % 10)
     | x when x / 10 = 8 -> "eighty"::toWords (x % 10)
     | x when x / 10 = 9 -> "ninety"::toWords (x % 10)
-    | x when x % 1000 = 0 -> toWords (x / 1000)@["thousand"]
-    | x when x % 100 = 0 -> toWords (x / 100)@["hundred"]
-    | x when x > 100 -> toWords (100 * (x / 100))@["and"]@toWords (x % 100)
+    | x when x >= 1000 -> toWords (x / 1000)@["thousand"]
+    | x when x >= 100 ->
+        let hundreds = toWords (x / 100)@["hundred"]
+        let rest = toWords (x % 100)
+        if rest.Length > 0 then hundreds@["and"]@rest else hundreds
 
 [1..1000]
 |> List.map toWords
